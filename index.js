40d67c09861808895ded20b5f4b6aed74a0b6df4
David Murcek <dfmurcek@gmail.com>
2020-04-01 16:24:35 -0700

    Changed greeting

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there'})
+    res.send({ bye: 'douche canoe'})
 });
 
 const PORT = process.env.PORT || 5000;
