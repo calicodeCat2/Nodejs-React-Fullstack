2a9d166f2059b1bf9d08b8dd25158095e1d326f4
David Murcek <dfmurcek@gmail.com>
2020-04-08 13:32:34 -0700

    Added billing and client side app

--- models/User.js
+++ models/User.js
@@ -5,6 +5,11 @@ const userSchema = new Schema({
     googleId: {
         type: String,
         required: true
+    },
+    credits: {
+        type: Number,
+        default: 0,
+        required: true
     }
 });
 
